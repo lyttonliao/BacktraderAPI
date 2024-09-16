7c2cc618d4a9f0b5cf51523b03489d35359cd577
Lytton Liao <lytton.liao@gmail.com>
2024-09-16 19:43:57 -0400

    Service handler now returns plots for each indicator, fixed import paths

--- app/analytics/vectorbt_service.py
+++ app/analytics/vectorbt_service.py
@@ -26,27 +26,33 @@ def price_data(
 def add_rsi_indicator(close: List[int], window: int, entry: float = 20.00, exit: float = 80.00):
     """Add a relative strength index indicator to the custom strategy"""
 
-    rsi = vbt.RSI.run(close, window).rsi
-    entries = rsi < entry
-    exits = rsi > exit
+    rsi = vbt.RSI.run(close, window)
+    rsi_df = rsi.rsi
+    fig = rsi.plot(levels=(entry, exit))
+    entries = rsi_df < entry
+    exits = rsi_df > exit
 
-    return entries, exits
+    return entries, exits, fig
 
 
 def add_ma_indicator(close: List[int], window: Optional[int] = None, fast_window: Optional[int] = None, slow_window: Optional[int] = None):
     "Add a moving average index indicator to the custom strategy"
 
+    print(window, slow_window, fast_window)
     if window:
         ma = vbt.MA.run(close, window)
         entries = ma.ma_crossed_above(close)
         exits = ma.ma_crossed_below(close)
+        fig = ma.plot()
     else:
         fast_ma = vbt.MA.run(close, fast_window)
         slow_ma = vbt.MA.run(close, slow_window)
         entries = fast_ma.ma_crossed_above(slow_ma.ma)
         exits = fast_ma.ma_crossed_below(slow_ma.ma)
+        fig = fast_ma.plot(ma_trace_kwargs=dict(name=f'Fast MA ({fast_window})'))
+        fig = slow_ma.plot(fig=fig, ma_trace_kwargs=dict(name=f'Slow MA ({slow_window})'))
 
-    return exits, entries
+    return exits, entries, fig
 
 
 def add_stoch_indicator(close: List[int], high: List[int], low: List[int], k_window: int, d_window: int, entry: int = 20, exit: int = 80):
@@ -57,7 +63,8 @@ def add_stoch_indicator(close: List[int], high: List[int], low: List[int], k_win
     stoch_d = stoch.percent_d
     entries = stoch.percent_k_crossed_above(stoch_d) | stoch_k < entry
     exits = stoch.percent_k_crossed_below(stoch_d) | stoch_k > exit
-    return entries, exits
+    fig = stoch.plot(levels=(entry, exit))
+    return entries, exits, fig
 
 
 def add_bbands_indicator(close: List[int], window: int = 20, alpha: int = 2):
@@ -70,7 +77,9 @@ def add_bbands_indicator(close: List[int], window: int = 20, alpha: int = 2):
     upper_shifted = bbands.upper.shift(-1)
     entries = bbands.close_below(lower) & bbands.close_above(lower_shifted)
     exits = bbands.close_above(upper) & bbands.close_below(upper_shifted)
-    return entries, exits
+    fig = bbands.plot()
+    fig.show()
+    return entries, exits, fig
 
 
 def add_atr_indicator(close: List[int], high: List[int], low: List[int], window: int = 14, trailing_stop: int = 2):
@@ -83,7 +92,8 @@ def add_atr_indicator(close: List[int], high: List[int], low: List[int], window:
     trigger_shifted = trigger.shift(1)
     entries = atr.close_crossed_above(trigger_shifted)
     exits = atr.close_crossed_below(stop_limit)
-    return entries, exits
+    fig = atr.plot()
+    return entries, exits, fig
 
 
 indicator_map = {
@@ -113,13 +123,15 @@ def run_strategy_handler(
 
     entries = pd.DataFrame()
     exits = pd.DataFrame()
+    figs = []
 
     for ind_type, inds in data.items():
         fn = indicator_map[ind_type]
 
         for ind in inds:
             inputs = [prices[input_name] for input_name in ind['inputs']]
-            new_entries, new_exits = fn(*inputs, **ind['params'])
+            new_entries, new_exits, fig = fn(*inputs, **ind['params'])
+            figs.append(fig)
             if entries.empty and exits.empty:
                 entries = new_entries
                 exits = new_exits
@@ -129,51 +141,58 @@ def run_strategy_handler(
 
     pf = vbt.Portfolio.from_signals(prices["Close"], entries, exits)
 
-    return pf.total_return()
+    return pf, figs
 
-# symbol = "TSLA"
-# end_time = datetime.datetime.now()
-# start_time = end_time - datetime.timedelta(days=365)
-# data = {
-#     'rsi': [
-#         {
-#             'inputs': ['Close'],
-#             'params': {
-#                 'exit': 80,
-#                 'entry': 20,
-#                 'window': 20
-#             }
-#         },
-#     ],
-#     'ma': [
-#         {
-#             'inputs': ['Close'],
-#             'params': {
-#                 'window': 20
-#             },
-#         },
-#         {
-#             'inputs': ['Close'],
-#             'params': {
-#                 'window': 50,
-#                 'fast_window': 50,
-#                 'slow_window': 10
-#             },
-#         }
-#     ],
-#     'stoch': [
-#         {
-#             'inputs': ['Close', 'Low', 'High'],
-#             'params': {
-#                 'entry': 20,
-#                 'exit': 80,
-#                 'k_window': 14,
-#                 'd_window': 3,
-#             }
-#         }
-#     ],
-#     'bbands': [],
-#     'atr': [],
-# }
-# 
-# print(run_strategy_handler(symbol=symbol, start_time=start_time, data=data))
+symbol = "TSLA"
+end_time = datetime.datetime.now()
+start_time = end_time - datetime.timedelta(days=365)
+data = {
+    'rsi': [
+        # {
+        #     'inputs': ['Close'],
+        #     'params': {
+        #         'exit': 80,
+        #         'entry': 20,
+        #         'window': 20
+        #     }
+        # },
+    ],
+    'ma': [
+        # {
+        #     'inputs': ['Close'],
+        #     'params': {
+        #         'window': 20
+        #     },
+        # },
+        # {
+        #     'inputs': ['Close'],
+        #     'params': {
+        #         'fast_window': 10,
+        #         'slow_window': 30
+        #     },
+        # }
+    ],
+    'stoch': [
+        # {
+        #     'inputs': ['Close', 'Low', 'High'],
+        #     'params': {
+        #         'entry': 20,
+        #         'exit': 80,
+        #         'k_window': 14,
+        #         'd_window': 3,
+        #     }
+        # }
+    ],
+    'bbands': [
+        {
+            'inputs': ['Close'],
+            'params': {
+                'window': 20,
+                'alpha': 1.5
+            }
+        }
+    ],
+    'atr': [],
+}
+
+print(run_strategy_handler(symbol=symbol, start_time=start_time, data=data))

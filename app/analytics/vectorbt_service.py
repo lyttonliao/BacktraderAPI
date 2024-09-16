fca7207bfd52ab2aa623103e27cb5621c4c4e7f9
Lytton Liao <lytton.liao@gmail.com>
2024-09-16 00:46:49 -0400

    Created function to combine signals from different indicators

--- app/analytics/vectorbt_service.py
+++ app/analytics/vectorbt_service.py
@@ -7,70 +7,83 @@ from typing import Optional, Dict, List
 
 
 def price_data(
-    symbols: List[str], 
+    symbol: str, 
     start_time: str, 
     end_time: str, 
     interval: str,
-    input_names: List[str]
 ):
     """Downloads data Yahoo Finance through VectorBT's API"""
-
-    data = vbt.YFData.download(
-        symbols=symbols,
+    yf_data = vbt.YFData.download_symbol(
+        symbol,
+        interval,
         start=start_time,
         end=end_time,
-        missing_index='drop',
-        interval=interval).get(*input_names)
-    
-    return data
+    )
+
+    return yf_data
 
 
-def add_rsi_indicator(close: List[int], window: int, trend = None, entry: int = 20, exit: int = 80):
+def add_rsi_indicator(close: List[int], window: int, entry: float = 20.00, exit: float = 80.00):
     """Add a relative strength index indicator to the custom strategy"""
 
-    rsi = vbt.RSI.run(close, window=window).rsi.to_numpy()
-    trend = np.where(rsi > exit, -1, trend if trend else 0)
-    trend = np.where(rsi < entry, 1, trend)
-    return trend
+    rsi = vbt.RSI.run(close, window).rsi
+    entries = rsi < entry
+    exits = rsi > exit
+
+    return entries, exits
 
 
-def add_ma_indicator(close: List[int], window: int, trend = None):
+def add_ma_indicator(close: List[int], window: Optional[int] = None, fast_window: Optional[int] = None, slow_window: Optional[int] = None):
     "Add a moving average index indicator to the custom strategy"
 
-    ma = vbt.MA.run(close, window=window).ma.to_numpy()
-    trend = np.where(close < ma, 1, trend if trend else 0)
-    return trend
+    if window:
+        ma = vbt.MA.run(close, window)
+        entries = ma.ma_crossed_above(close)
+        exits = ma.ma_crossed_below(close)
+    else:
+        fast_ma = vbt.MA.run(close, fast_window)
+        slow_ma = vbt.MA.run(close, slow_window)
+        entries = fast_ma.ma_crossed_above(slow_ma.ma)
+        exits = fast_ma.ma_crossed_below(slow_ma.ma)
+
+    return exits, entries
 
 
-def add_stoch_indicator(close: List[int], high: List[int], low: List[int], k_window: int, d_window: int, trend = None, entry: int = 20, exit: int = 80):
+def add_stoch_indicator(close: List[int], high: List[int], low: List[int], k_window: int, d_window: int, entry: int = 20, exit: int = 80):
     """Add a stochastic oscillator indicator to the custom strategy"""
 
-    stoch = vbt.STOCH.run(close, high=high, low=low, k_window=k_window, d_window=d_window).stoch.to_numpy()
-    trend = np.where(stoch > exit, -1, trend if trend else 0)
-    trend = np.where(stoch < entry, 1, trend)
-    return trend
+    stoch = vbt.STOCH.run(close, high, low, k_window, d_window)
+    stoch_k = stoch.percent_k
+    stoch_d = stoch.percent_d
+    entries = stoch.percent_k_crossed_above(stoch_d) | stoch_k < entry
+    exits = stoch.percent_k_crossed_below(stoch_d) | stoch_k > exit
+    return entries, exits
 
 
-def add_bbands_indicator(close: List[int], window: int = 20, trend = None, alpha: int = 2):
+def add_bbands_indicator(close: List[int], window: int = 20, alpha: int = 2):
     """Add a bollinger bands indicator to the custom strategy, currently supports basic bollinger actions, does not trace contraction or expansion of the bands"""
 
-    bbands = vbt.BBANDS.run(close, window=window, alpha=alpha).bbands.to_numpy()
-    upper_band = bbands[:, 0]
-    middle_band = bbands[:, 1]
-    lower_band = bbands[:, 2]
-    trend = np.where((lower_band < close < middle_band) | (close > upper_band), -1, trend if trend else 0)
-    trend = np.where((middle_band < close < upper_band) | (close < lower_band), 1, trend)
-    return trend
+    bbands = vbt.BBANDS.run(close, window, alpha)
+    lower = bbands.lower
+    lower_shifted = bbands.lower.shift(-1)
+    upper = bbands.upper
+    upper_shifted = bbands.upper.shift(-1)
+    entries = bbands.close_below(lower) & bbands.close_above(lower_shifted)
+    exits = bbands.close_above(upper) & bbands.close_below(upper_shifted)
+    return entries, exits
 
 
-def add_atr_indicator(close: List[int], high: List[int], low: List[int], window: int = 14, trend = None):
+def add_atr_indicator(close: List[int], high: List[int], low: List[int], window: int = 14, trailing_stop: int = 2):
     """Add an average true range indicator to the custom strategy"""
 
-    atr = vbt.ATR.run(close, high=high, low=low, window=window).atr.to_numpy()
-    trigger_price = close + atr
-    entries = close.shift(-1)
-    trend = np.where((entries > trigger_price), 1, trend if trend else 0)
-    return trend
+    atr = vbt.ATR.run(close, high, low, window)
+    atr_val = atr.atr
+    trigger = close + atr_val
+    stop_limit = close - atr_val * trailing_stop
+    trigger_shifted = trigger.shift(1)
+    entries = atr.close_crossed_above(trigger_shifted)
+    exits = atr.close_crossed_below(stop_limit)
+    return entries, exits
 
 
 indicator_map = {
@@ -81,108 +94,86 @@ indicator_map = {
     'atr': add_atr_indicator
 }
 
-def custom_indicator(prices, ind_dict):
-    """Creates a custom indicator by combining many indicators with different parameters"""
-
-    trend = None
-    for ind_type, inds in ind_dict.items():
-        fn = indicator_map[ind_type]
-        for ind in inds:
-            inputs = [prices[input_name] for input_name in ind['inputs']] if len(ind['inputs']) > 1 else prices
-            trend = fn(*inputs, **ind['params'], trend=trend)
-            
-    return trend
-
-# shape of data
-# {
-#     'rsi': [
-#         {
-#               'inputs': ['Close'],
-#               'params': {
-#                   'entry': 80,
-#                   'exit': 20,
-#                   'window': 20
-#               }
-#         },
-#     ]
-# }
 
 def run_strategy_handler(
-    symbols: List[str],
+    symbol: List[str],
     start_time: str,
     data: Dict[str, List[Dict]],
     end_time: str = datetime.datetime.now(),
-    interval: str = '1m',
-    class_name: str = 'Custom Indicator',
-    output_names: List[str] = ['value'],
+    interval: str = 'max',
 ):
     """Use VectorBT's Indicator Factory API to construct a custom indicator/strategy"""
 
-    input_names = ['close']
-
-    if len(data['stoch']) > 0 or len(data['atr']) > 0:
-        input_names.extend(['low', 'high'])
-
-    ind = vbt.IndicatorFactory(
-        class_name = class_name,
-        input_names = input_names,
-        param_names = ['ind_dict'],
-        output_names = output_names
-    ).from_apply_func(
-        custom_indicator
-    )
-
     prices = price_data(
-        symbols=symbols,
+        symbol=symbol,
         start_time=start_time,
         end_time=end_time,
         interval=interval,
-        input_names=map(lambda s: s.capitalize(), input_names)
     )
 
-    res = ind.run(
-        prices,
-        ind_dict=data
-    )
+    entries = pd.DataFrame()
+    exits = pd.DataFrame()
 
-    entries = res.value == 1.0
-    exits = res.value == -1.0
+    for ind_type, inds in data.items():
+        fn = indicator_map[ind_type]
 
-    pf = vbt.Portfolio.from_signals(prices, entries, exits)
+        for ind in inds:
+            inputs = [prices[input_name] for input_name in ind['inputs']]
+            new_entries, new_exits = fn(*inputs, **ind['params'])
+            if entries.empty and exits.empty:
+                entries = new_entries
+                exits = new_exits
+            else:
+                entries = entries & new_entries
+                exits = exits & new_exits
 
-    return pf.total_returns()
+    pf = vbt.Portfolio.from_signals(prices["Close"], entries, exits)
 
-symbols = ["TSLA"]
-end_time = datetime.datetime.now()
-start_time = end_time - datetime.timedelta(days=2)
-data = {
-    'rsi': [
-        {
-            'inputs': ['close'],
-            'params': {
-                'entry': 80,
-                'exit': 20,
-                'window': 20
-            }
-        },
-    ],
-    'ma': [
-        {
-            'inputs': ['close'],
-            'params': {
-                'window': 20
-            },
-        },
-        {
-            'inputs': ['close'],
-            'params': {
-                'window': 50
-            },
-        }
-    ],
-    'stoch': [],
-    'bbands': [],
-    'atr': [],
-}
+    return pf.total_return()
 
-print(run_strategy_handler(symbols=symbols, start_time=start_time, data=data))
+# symbol = "TSLA"
+# end_time = datetime.datetime.now()
+# start_time = end_time - datetime.timedelta(days=365)
+# data = {
+#     'rsi': [
+#         {
+#             'inputs': ['Close'],
+#             'params': {
+#                 'exit': 80,
+#                 'entry': 20,
+#                 'window': 20
+#             }
+#         },
+#     ],
+#     'ma': [
+#         {
+#             'inputs': ['Close'],
+#             'params': {
+#                 'window': 20
+#             },
+#         },
+#         {
+#             'inputs': ['Close'],
+#             'params': {
+#                 'window': 50,
+#                 'fast_window': 50,
+#                 'slow_window': 10
+#             },
+#         }
+#     ],
+#     'stoch': [
+#         {
+#             'inputs': ['Close', 'Low', 'High'],
+#             'params': {
+#                 'entry': 20,
+#                 'exit': 80,
+#                 'k_window': 14,
+#                 'd_window': 3,
+#             }
+#         }
+#     ],
+#     'bbands': [],
+#     'atr': [],
+# }
+# 
+# print(run_strategy_handler(symbol=symbol, start_time=start_time, data=data))
